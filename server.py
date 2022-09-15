77f4aa9f1b597e0438294dc1837ebe7f46f4ed2e
ilimdar.muratov2001 <ilimdar.muratov2001@gmail.com>
2022-09-15 20:15:16 +0300

    First commit

---
+++ server.py
@@ -0,0 +1,30 @@
+from requests.auth import  HTTPBasicAuth
+from flask import Flask, Response
+import cv2
+
+class VideoCamera(object):
+    def __init__(self):
+        self.cap = cv2.VideoCapture(0) 
+    
+    def __del__(self):
+        self.cap.release()
+    
+    def get_frame(self):
+        success, image = self.cap.read()
+        ret, jpeg = cv2.imencode('.jpg', image)
+        return jpeg.tobytes()
+
+def gen(camera):
+    while True:
+        frame = camera.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
+app = Flask(__name__)
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen(VideoCamera()), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if __name__=="__main__":
+	app.run()
